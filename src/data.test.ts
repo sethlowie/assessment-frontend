62a54c39d48327d299a3a84306c409c3b7dcccc6
Seth Lowie <seth.lowie@couchbase.com>
2023-02-14 10:46:19 -0500

    format test file

--- src/data.test.ts
+++ src/data.test.ts
@@ -1,9 +1,9 @@
 import page1Data from "../server/pages/pageOne";
 
-import { pageSchema} from "./data";
+import { pageSchema } from "./data";
 
 describe("data", () => {
     it("should parse", () => {
-        pageSchema.parse(page1Data)
+        pageSchema.parse(page1Data);
     });
 });
