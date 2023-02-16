4f89b29799c4852c79eadc00ec4f3c7ed3a98971
Seth Lowie <seth.lowie@couchbase.com>
2023-02-16 06:29:56 -0500

    move files into an api folder

--- src/data.test.ts
+++
@@ -1,9 +0,0 @@
-import page1Data from "../server/pages/pageOne";
-
-import { pageSchema } from "./data";
-
-describe("data", () => {
-    it("should parse", () => {
-        pageSchema.parse(page1Data);
-    });
-});
