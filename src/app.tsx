3d5eb19d08abc90b70c75f27f51e74980e690301
Seth Lowie <seth.lowie@couchbase.com>
2023-02-16 06:30:26 -0500

    add spacing to cards

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,22 @@
 import { useParams } from "react-router";
-import { useGetPageInfoQuery } from "./api";
+import { useGetPageInfoQuery, ComponentSchema } from "./api";
 import { Image } from "./components/Image";
 import { Weather } from "./components/Weather";
+import { Loader } from './components/Loader';
 import { assertUnreachable } from "./utils";
+import React from "react";
+import styles from "./app.module.css";
+
+const renderComponent = (component: ComponentSchema): React.ReactChild => {
+    switch (component.type) {
+        case "image":
+            return <Image src={component.options.src} alt={component.options.alt}></Image>;
+        case "weather":
+            return <Weather c={component} />;
+        default:
+            return assertUnreachable(component);
+    }
+};
 
 const App = () => {
     const { id } = useParams<{ id: string }>();
@@ -11,30 +25,19 @@ const App = () => {
 
     if (r.isSuccess) {
         return (
-            <div>
-                <div>
-                    {r.data.data.lists[0].components.map((id) => {
-                        const cmp = r.data.data.components.find((c) => c.id === id);
-                        if (!cmp) {
-                            return <span>Not Found</span>;
-                        }
+            <div className={styles.Wrapper}>
+                {r.data.data.lists[0].components.map((id) => {
+                    const component = r.data.data.components.find((c) => c.id === id);
+                    if (!component) {
+                        return <span>Not Found</span>;
+                    }
 
-                        switch (cmp.type) {
-                            case "image":
-                                return <Image src={cmp.options.src} alt={cmp.options.alt}></Image>;
-                            case "weather":
-                                return <Weather />;
-                            default:
-                                return assertUnreachable(cmp.type);
-                        }
-                    })}
-                </div>
+                    return renderComponent(component);
+                })}
             </div>
         );
     }
-    return <span>waiting</span>
-    // return <pre>{JSON.stringify(r, null, 2)}</pre>;
-    // return <div>Render {id}</div>;
+    return <Loader />
 };
 
 export default App;
