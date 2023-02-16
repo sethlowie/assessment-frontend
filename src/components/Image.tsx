96226a2e993ee48272c7a13f1dee9d3ce50961c3
Seth Lowie <seth.lowie@couchbase.com>
2023-02-16 06:29:15 -0500

    style image as a card

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,5 +1,13 @@
-import type { Image as ImageType } from "../data";
+import type { Image as ImageType } from "../api";
+import { Card } from "./Card";
+import styles from './Image.module.css';
 
 export const Image = ({ src, alt }: ImageType["options"]) => {
-    return <img src={src} alt={alt} />;
+    return (
+        <Card fullWidth>
+        <div className={styles.Container}>
+            <img src={src} alt={alt} className={styles.Img} />
+        </div>
+        </Card>
+    );
 };
